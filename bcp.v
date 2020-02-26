c7d31cb3a23849be1e5524cd7202d9616079967c
Herman Bergwerf <hermanbergwerf@gmail.com>
2020-02-26 22:52:05 +0100

    Add lpo_llpo and bcp_nlpo

--- bcp.v
+++ bcp.v
@@ -3,6 +3,8 @@
 From intuitionism Require Import seq.
 
 Require Import Coq.Arith.PeanoNat.
+Require Import Coq.Logic.ConstructiveEpsilon.
+Require Import Omega.
 Import Nat.
 
 (* Brouwers Continuity Principle *)
@@ -16,24 +18,53 @@ Axiom BCP : BCP.
 End Brouwer.
 
 (* Limited Principle of Omniscience *)
-Definition LPO := forall (α : seq), (forall n, α n = 0) \/ (exists n, α n <> 0).
+Definition LPO := forall (α : seq), (exists n, α n <> 0) \/ (forall n, α n = 0).
 
 (* Lesser Limited Principle of Omniscience *)
 Definition LLPO := forall (α : seq),
   (forall k, ((forall i, i < k -> α i = 0) /\ α k <> 0) -> Even k) \/
   (forall k, ((forall i, i < k -> α i = 0) /\ α k <> 0) -> Odd k).
 
-Lemma lpo_llpo :
+Theorem lpo_llpo :
   LPO -> LLPO.
 Proof.
-Admitted.
+assert(nltgt_eq: forall n m : nat, ~(n < m) /\ ~(n > m) -> n = m).
+{ intros; omega. }
+intros LPO α.
+assert(neq0_dec: forall n, {α n <> 0} + {~(α n <> 0)}).
+{ intros; destruct (eq_dec (α n) 0). right; omega. left; omega. }
+destruct (LPO α).
+- destruct (epsilon_smallest _ neq0_dec H) as [l [L1 L2]].
+  destruct (even l) eqn:E.
+  + apply even_spec in E; left; intros k [H1 H2].
+    assert(Hk: k = l). apply nltgt_eq; split; intros P.
+    * apply L2 in P; auto.
+    * apply H1 in P; auto.
+    * subst; auto.
+  + assert(O: Odd l). apply odd_spec; unfold odd; rewrite E; auto.
+    right; intros k [H1 H2].
+    assert(Hk: k = l). apply nltgt_eq; split; intros P.
+    * apply L2 in P; auto.
+    * apply H1 in P; auto.
+    * subst; auto.
+- left; intros k [H1 H2]; exfalso; auto.
+Qed.
 
-Lemma bcp_or_lpo :
-  BCP -> ~LPO /\ LPO -> ~BCP.
+Theorem bcp_nlpo :
+  BCP -> ~LPO.
 Proof.
-Admitted.
-
-Lemma bcp_or_llpo :
-  BCP -> ~LLPO /\ LLPO -> ~BCP.
-Proof.
-Admitted.
+intros BCP LPO.
+assert(R: forall α : seq, exists i,
+  (i = 0 /\ exists n, α n <> 0) \/
+  (i > 0 /\ forall n, α n = 0)).
+{ intros; destruct (LPO α). exists 0; left; auto. exists 1; right; auto. }
+destruct (BCP _ R (0..)) as [m [n H]]. destruct (eq_dec n 0) as [n0|n1].
+- assert(Hα : con m (0..) (0..)). apply con_id.
+  apply H in Hα as [[_ [i E]]|[n1 _]]; try omega.
+  apply E; auto.
+- pose (β := (prepend m (0..) (1..))).
+  assert(Hβ: con m (0..) β). apply con_prepend.
+  apply H in Hβ as [[n0 _]|[_ A]]; try omega.
+  assert(Hβ1: β (m + 1) <> 0). unfold β, cseq; rewrite prepend_unshift; omega.
+  auto.
+Qed.
--- seq.v
+++ seq.v
@@ -3,6 +3,7 @@
 Require Import Coq.Init.Nat.
 Require Import Coq.Bool.Bool.
 Require Import Coq.Lists.List.
+Require Import Coq.Logic.FunctionalExtensionality.
 Require Import Omega.
 Import Nat.
 Import ListNotations.
@@ -26,82 +27,131 @@ Definition con n (α β : seq) := forall i, i < n -> α i = β i.
 (* Apartness relation. *)
 Definition apart (α β : seq) := exists n, α n <> β n.
 
-(* Shift a sequence n elements forward. *)
-Definition shift n (α : seq) i := α (i - n).
+(* Delete first n elements. *)
+Definition del n (α : seq) i := α (i + n).
+
+(* Prepend n times (α 0) to the sequence. *)
+Definition fill n (α : seq) i := α (i - n).
 
 (* Overwrite the first n elements of α with β. *)
 Definition replace n (α β : seq) i := if i <? n then β i else α i.
 
 (* Prepend first n elements of α to β. *)
-Definition prepend n α β := replace n (shift n β) α.
+Definition prepend n α β := replace n (fill n β) α.
 
 (* Check if α starts with s. *)
 Fixpoint starts s (α : seq) := 
   match s with
   | [] => True
-  | s0 :: t => α 0 = s0 /\ starts t (shift 1 α)
+  | s0 :: t => α 0 = s0 /\ starts t (del 1 α)
   end.
 
 (* Get first n elements of α. *)
 Fixpoint get n (α : seq) : fseq :=
   match n with
   | 0 => []
-  | S m => (α 0) :: (get m (shift 1 α))
+  | S m => (α 0) :: (get m (del 1 α))
   end.
 
 Notation "α '#' β" := (apart α β) (at level 50, format "α '#' β").
-Notation "α '..' n" := (get n α) (at level 10, format "α '..' n").
+Notation "α '@' n" := (get n α) (at level 30, format "α '@' n").
 Notation "s '⊏' α" := (starts s α) (at level 70).
+Notation "c '..'" := (cseq c) (at level 10, format "c '..'").
 
 Lemma app_split (a b x y : fseq) : a = b -> x = y -> a ++ x = b ++ y.
 Proof. intros; subst; auto. Qed.
 
-(* Shifting withing the coincedence. *)
-Lemma con_shift n m α β :
-  con (n + m) α β <-> con m (shift n α) (shift n β).
+(* A sequence coincides with itself. *)
+Lemma con_id n α : con n α α.
+Proof. intros i; auto. Qed.
+
+(* A smaller part of a coincedence also coincides. *)
+Lemma con_leq n m α β : con (n + m) α β -> con n α β.
+Proof. intros H i Hi; apply H; omega. Qed.
+
+(* Delete part of a coincedence. *)
+Lemma con_del n m α β :
+  con (n + m) α β <-> con n α β /\ con m (del n α) (del n β).
 Proof.
-Admitted.
+split; unfold del; simpl.
+- intros H; split. eapply con_leq; apply H.
+  intros i Hi; apply H; omega.
+- intros [H1 H2] i Hi. assert(C: i < n \/ i >= n). omega.
+  destruct C. apply H1; auto. assert(R: i = (i - n) + n). omega.
+  rewrite R; apply H2; omega.
+Qed.
 
 (* α and β coincide iff their first n elements are the same. *)
 Lemma con_eq_get n α β :
-  con n α β <-> α..n = β..n.
+  con n α β <-> α@n = β@n.
 Proof.
 split; revert α β.
 - induction n; simpl; auto; intros.
-  erewrite H, IHn; auto; try omega; apply con_shift; auto.
+  erewrite H, IHn; auto; try omega; apply con_del; auto.
 - induction n; simpl; intros. intros i Hi; omega.
-  rewrite <-add_1_r, add_comm; apply con_shift, IHn; inversion_clear H; auto.
+  rewrite <-add_1_r, add_comm; apply con_del; split; intros i Hi.
+  + assert(I: i = 0). omega. subst; inversion_clear H; auto.
+  + apply IHn; inversion_clear H; auto.
 Qed.
 
-(* Addition distributes over get/shift/append. *)
-Lemma add_shift_distr n m α :
-  α..(n + m) = α..n ++ (shift n α)..m.
+(* Delete 0 elements. *)
+Lemma del0 α : del 0 α = α.
 Proof.
-Admitted.
+apply functional_extensionality.
+intros; unfold del; rewrite add_0_r; auto.
+Qed.
+
+(* Fill 0 elements. *)
+Lemma fill0 α : fill 0 α = α.
+Proof.
+apply functional_extensionality.
+intros; unfold fill; rewrite sub_0_r; auto.
+Qed.
+
+(* Repeated deletion *)
+Lemma del_add_distr n m α :
+  del n (del m α) = del (n + m) α.
+Proof.
+unfold del; apply functional_extensionality.
+intros; rewrite add_assoc; auto.
+Qed.
+
+(* Deletion and append *)
+Lemma del_app_distr n m α :
+  α@(n + m) = α@n ++ (del n α)@m.
+Proof.
+revert α. induction n, m; simpl; intros; auto.
+- rewrite del0; auto.
+- rewrite add_0_r, app_nil_r; auto.
+- rewrite IHn; simpl; rewrite <-(add_1_r n), ?del_add_distr; auto.
+Qed.
 
 (* Different length parts are never equal. *)
 Lemma get_neq n m α β :
-  n <> m -> α..n <> β..m.
+  n <> m -> α@n <> β@m.
 Proof.
 Admitted.
 
 (* Get finite part of a partially constant sequence. *)
 Lemma get_cseq_eq_cfseq c n α :
-  (con n α (cseq c)) <-> α..n = cfseq c n.
+  (con n α (cseq c)) <-> α@n = cfseq c n.
 Proof.
 Admitted.
 
-(* A prepended constant part coincides with a constant sequence. *)
-Lemma get_prepend_eq_repeat n c α :
-  con n (cseq c) (prepend n (cseq c) α).
+(* A prepended sequence coincides with itself. *)
+Lemma con_prepend n α β :
+  con n α (prepend n α β).
 Proof.
-Admitted.
+intros i Hi; unfold prepend, replace, fill.
+apply ltb_lt in Hi; rewrite Hi; auto.
+Qed.
 
-
-
-
-
-
-
-
-
+(* Access elements after prepend *)
+Lemma prepend_unshift n m α β :
+  (prepend n α β) (n + m) = β m.
+Proof.
+unfold prepend, replace, fill.
+assert(R1: n + m <? n = false). apply ltb_ge; omega.
+assert(R2: n + m - n = m). omega.
+rewrite R1, R2; auto.
+Qed.
