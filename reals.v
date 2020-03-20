76ea934398e665bf8be7dbd7713336aad0ff460a
Herman Bergwerf <hermanbergwerf@gmail.com>
2020-03-20 20:51:24 +0100

    Edit reals.v

--- reals.v
+++ reals.v
@@ -1,6 +1,7 @@
 (* Some results about real numbers *)
 
-From intuitionism Require Import lib set seq spr fan func choice bcp bar.
+From intuitionism Require Import lib set seq spr fan func classic choice.
+From intuitionism Require Import bcp bar.
 
 (*
 To formalize some interesting results about real numbers this document defines
@@ -15,24 +16,45 @@ half would require exact knowledge about the upper/lower bound of some numbers,
 which is a problem for some of the constructions.
 *)
 
-(* Compute left endpoint of the n-th interval of α with denominator 3^n. *)
+(* Compute left endpoint of the n-th interval of α with denominator 3^(n+1). *)
 Fixpoint lbound α n :=
+  (if α n =? 0 then 0 else 2^n) +
   match n with
   | 0 => 0
-  | S m => let l := lbound (del 1 α) m in if α 0 =? 0 then l else 3^m + l
+  | S m => 3 * lbound α m
   end.
 
 (* Distance between m and n. *)
 Definition distance m n := (m - n) + (m - n).
 
 (* α and β are within distance 1/(2^δ) of each other. *)
-Definition within (α β : dom Bin) (δ : nat) :=
+Definition within (δ : nat) (α β : dom Bin) :=
   ∃n, 2^δ * distance (lbound α n) (lbound β n) < 3^n.
 
-(* Pointwise continuity of f : [0,1] -> [0,1] at x0. *)
-Definition point_continuous f x₀ ε :=
-  ∃δ, ∀x, within x₀ x δ -> within (f x₀) (f x) ε.
+(* Continuity of f : [0,1] -> [0,1] at x. *)
+Definition point_continuous f x ε :=
+  ∃δ, ∀x', within δ x x' -> within ε (f x) (f x').
+
+(* Pointwise continuity of f : [0,1] -> [0,1]. *)
+Definition pointwise_continuous f :=
+  ∀x ε, point_continuous f x ε.
 
 (* Uniform continuity of f : [0,1] -> [0,1]. *)
-Definition uniform_continuous f ε :=
-  ∃δ, ∀x₀ x₁, within x₀ x₁ δ -> within (f x₀) (f x₁) ε.
+Definition uniform_continuous f :=
+  ∀ε, ∃δ, ∀x x', within δ x x' -> within ε (f x) (f x').
+
+(* The intermediate value theorem. *)
+Definition IntermediateValueTheorem :=
+  ∀f, pointwise_continuous f /\ f (0^ω) = 0^ω /\ f (1^ω) = 1^ω ->
+  ∀y, ∃x, f x = y.
+
+(* Simple form of Brouwers fixed-point theorem. *)
+Definition FixedPointTheorem :=
+  ∀f, uniform_continuous f -> ∃x, f x = x.
+
+(*
+It would be nice if we can show that both the intermediate value theorem and
+Brouwers fixed-point theorem imply LPO without resorting to a full definition
+of real numbers. However it seems the binary sequences are not particularly
+easy to reason about either, or do arithmetic with.
+*)
